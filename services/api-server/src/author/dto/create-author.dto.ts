2ee106d2ed55d637d325522a016c880a033bd0ff
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-06-20 21:14:03 -0400

    Add decorators for Swagger

--- services/api-server/src/author/dto/create-author.dto.ts
+++ services/api-server/src/author/dto/create-author.dto.ts
@@ -1,5 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateAuthorDto {
+  @ApiProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
+
+  @ApiProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
+
+  @ApiProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
   handle: string;
 }
