634c07be1b507d8c1d102144fa7475ef8c16092e
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-06-20 21:13:33 -0400

    Add modules to App module imports

--- services/api-server/src/app.module.ts
+++ services/api-server/src/app.module.ts
@@ -3,9 +3,18 @@ import { ConfigModule } from "@nestjs/config";
 import { DummyModule } from "@/dummy/dummy.module";
 import { InitModule } from "@/init/init.module";
 import { AuthorModule } from "@/author/author.module";
+import { BookModule } from "./book/book.module";
+import { ChapterModule } from "./chapter/chapter.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), InitModule, DummyModule, AuthorModule],
+  imports: [
+    ConfigModule.forRoot(),
+    InitModule,
+    DummyModule,
+    AuthorModule,
+    BookModule,
+    ChapterModule,
+  ],
   controllers: [],
   providers: [],
 })
