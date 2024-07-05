8719fc54ba5a0dadd1a479cbbdde01fe56a42b94
michaellibucha <michaellibucha@gmail.com>
2024-07-05 15:09:47 -0700

    Michael/author modal - Fix build and lint issues. (#15)
    Co-authored-by: Michael Libucha <michaellibucha@users.noreply.github.com>

--- services/api-server/src/app.module.ts
+++ services/api-server/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { BookModule } from "./book/book.module";
 import { DummyModule } from "@/dummy/dummy.module";
 import { InitModule } from "@/init/init.module";
 import { AuthorModule } from "@/author/author.module";
+import { BookModule } from "./book/book.module";
 
 @Module({
   imports: [
--- services/api-server/src/book/book.module.ts
+++ services/api-server/src/book/book.module.ts
@@ -3,14 +3,14 @@ import {
   Module,
   type NestModule,
 } from "@nestjs/common";
-import { DbModule } from "../db/db.module";
-import { BookController } from "./book.controller";
-import { BookService } from "./book.service";
-import { bookProviders } from "./book.provider";
 import { AuthMiddleware } from "@/middleware/auth.middleware";
 import { AuthorModule } from "@/author/author.module";
 import { ChapterModule } from "@/chapter/chapter.module";
 import { chapterProviders } from "@/chapter/chapter.provider";
+import { DbModule } from "../db/db.module";
+import { BookController } from "./book.controller";
+import { BookService } from "./book.service";
+import { bookProviders } from "./book.provider";
 
 @Module({
   imports: [DbModule, AuthorModule, ChapterModule],
