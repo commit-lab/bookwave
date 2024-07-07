fdbefbd7cfc03eeead4c1378e4731aea3b5fa640
Tyler Bray <tylerhbray@gmail.com>
2024-07-07 12:57:56 -0700

    table of content styling

--- apps/web/src/features/books/components/table-of-contents.tsx
+++ apps/web/src/features/books/components/table-of-contents.tsx
@@ -1,24 +1,67 @@
 import React from "react";
-import Link from "next/link";
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemContent,
+  Sheet,
+} from "@mui/joy";
 import { dummyData } from "../dummy-data";
 
 export default function TableOfContents() {
   const book = dummyData.books[0];
   if (!book) {
-    return <div>Book not found</div>;
+    return <Typography level="h3">Book not found</Typography>;
   }
 
   return (
-    <div>
-      <h1>{book.title}</h1>
-      <h2>By {book.author}</h2>
-      <ul>
-        {book.chapters.map((chapter) => (
-          <li key={chapter.id}>
-            <Link href={`/books/${String(chapter.id)}`}>{chapter.title}</Link>
-          </li>
+    <Sheet
+      variant="outlined"
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        minHeight: "100vh",
+        paddingTop: 10,
+        px: 10,
+      }}
+    >
+      <div style={{ textAlign: "start", maxWidth: "50%" }}>
+        <Typography level="h2" component="h1" sx={{ mb: 2, fontSize: 40 }}>
+          {book.title}
+        </Typography>
+        <Typography sx={{ fontSize: 25 }} level="body-sm">
+          By {book.author}
+        </Typography>
+      </div>
+
+      <List
+        aria-labelledby="table-of-contents"
+        sx={{
+          "--List-decorator-size": "32px",
+          "--List-item-paddingLeft": "1rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "start",
+          maxWidth: "50%",
+          paddingLeft: 3,
+          fontSize: 25,
+          fontWeight: 500,
+        }}
+      >
+        {[
+          "Introduction",
+          ...book.chapters.map((chapter) => chapter.title),
+          "Conclusion",
+        ].map((title, index) => (
+          <ListItem key={index}>
+            <ListItemButton>
+              <ListItemContent>{title}</ListItemContent>
+            </ListItemButton>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </Sheet>
   );
 }
