242bd21b5fb38c60cc2eeba47e8922048c537cfc
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-07-10 23:58:25 -0700

    Fixed layout + styling

--- apps/web/src/features/admin/components/book-card.tsx
+++ apps/web/src/features/admin/components/book-card.tsx
@@ -13,46 +13,45 @@ interface BookCardProps {
 export default function BookCard({ book }: BookCardProps) {
   const theme = useTheme();
   return (
-    <Box key={book.id}>
-      <Card
+    <Card
+      key={book.id}
+      sx={{
+        minWidth: { xs: "100%", sm: 600, md: 800, lg: 1000 },
+        display: "flex",
+        flexDirection: { xs: "column", sm: "row" },
+        justifyContent: "space-between",
+        alignItems: "center",
+        ":hover": {
+          backgroundColor: theme.palette.background.backdrop,
+        },
+      }}
+    >
+      <CardContent
         sx={{
-          minWidth: { xs: "100%", sm: 600, md: 800, lg: 1000 },
           display: "flex",
-          flexDirection: { xs: "column", md: "row" },
+          flexDirection: "row",
           justifyContent: "space-between",
           alignItems: "center",
-          ":hover": {
-            backgroundColor: theme.palette.background.backdrop,
-          },
         }}
       >
-        <Link href={`/admin/book/${book.title}`}>
-          <CardContent
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Box sx={{ textAlign: "start" }}>
-              <Typography>Title</Typography>
-              <Typography>{book.title}</Typography>
-            </Box>
-            <Box>
-              <Typography>Chapters</Typography>
-              <Typography>{book.chapterCount}</Typography>
-            </Box>
-            <Box>
-              <Typography>State</Typography>
-              <Typography>{book.state}</Typography>
-            </Box>
-          </CardContent>
-        </Link>
-        <CardActions>
-          <BookOptions />
-        </CardActions>
-      </Card>
-    </Box>
+        <Box sx={{ textAlign: "start" }}>
+          <Link href={`/admin/book/${book.title}`}>
+            <Typography level="body-lg">Title</Typography>
+            <Typography level="h3">{book.title}</Typography>
+          </Link>
+        </Box>
+        <Box sx={{ textAlign: "center" }}>
+          <Typography level="body-lg">Chapters</Typography>
+          <Typography level="h3">{book.chapterCount}</Typography>
+        </Box>
+        <Box sx={{ textAlign: "end" }}>
+          <Typography level="body-lg">State</Typography>
+          <Typography level="h3">{book.state}</Typography>
+        </Box>
+      </CardContent>
+      <CardActions>
+        <BookOptions bookId={book.id} />
+      </CardActions>
+    </Card>
   );
 }
