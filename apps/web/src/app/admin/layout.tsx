d0e249d79572ba1412d81b1900a092eec585758c
Michael Libucha <michaellibucha@users.noreply.github.com>
2024-07-17 18:50:46 -0700

    Add loading for auth on all admin pages.

--- apps/web/src/app/admin/layout.tsx
+++ apps/web/src/app/admin/layout.tsx
@@ -4,6 +4,8 @@ import { Box } from "@mui/joy";
 import { useParams } from "next/navigation";
 import SideBar from "@/features/admin/components/sidebar";
 import TopBar from "@/features/admin/components/topbar";
+import { useIsSignedIn } from "@/features/auth/hooks/use-is-signed-in";
+import InitialLoading from "@/features/auth/components/initial-loading";
 
 export default function AdminLayout({
   children,
@@ -12,6 +14,12 @@ export default function AdminLayout({
 }) {
   const params = useParams();
   const isSpecificRoute = params.chapter_number;
+  const isSignedIn = useIsSignedIn();
+
+  if (!isSignedIn) {
+    return <InitialLoading />;
+  }
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       {!isSpecificRoute && <TopBar />}
